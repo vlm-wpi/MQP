da8aaafeb0af708b3936017e9c54d6e39d717d38
George Heineman <heineman@wpi.edu>
2022-01-21 13:39:48 -0500

    now runs headless with stats...

--- app/nodeApp.js
+++ app/nodeApp.js
@@ -7,11 +7,15 @@ global.data = in_data['data'];
 // configure as you want...
 global.data.max['AdultBackpack'] = 0;
 global.data.max['AdultBike'] = 0;
-global.data.max['Adult'] = 0;
+global.data.max['Adult'] = 500;
+global.data.max['Child'] = 0;
 
 // SKIP the GUI!
 global.gui = {}
 global.gui.headless = true;
+
+const pop = require('./pop');
+global.pop = pop['pop'];
 
 const heap = require('./heap');
 global.heap = heap['heap'];
@@ -19,18 +23,25 @@ global.heap = heap['heap'];
 const astar = require('./astar');
 global.astar = astar['astar'];
 
-const final = require('./final');
-global.final = astar['final'];
+const app = require('./final');
+global.app = astar['final'];
 const lzw = require('./LZWEncoder');
 const nq = require('./NeuQuant');
 const gife = require('./GIFEncoder');
 const b64 = require('./b64');
 
 function process_all() {
-   console.log(final.get_state().population);
+   console.log(app.final.get_state().population);
    console.log("DONE");
+   var things = global.pop.types();
+
+   for (i = 0; i < things.length; i++) {
+     var tpe = things[i];
+     console.log(tpe + ": " + app.final.collisions_total[tpe]);
+   }
 }
 
 // run for 100 steps...
-final.start_simulation(10, process_all);
+console.log(app);
+app.final.start_simulation(10, process_all);
 
