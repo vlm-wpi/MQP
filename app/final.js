102893281bf932136f9bdbeddde06eba245a0b19
Ally Salvino <62515404+amsalvino@users.noreply.github.com>
2022-01-31 16:21:44 -0500

    working on voronoi

--- app/final.js
+++ app/final.js
@@ -716,7 +716,6 @@ function State() {
 	} else if (diagonal) {
 	    heuristic = diagonald;
 	}
-
         var node = astar.AStar(state, thing, 0, heuristic); //using AStar algorithm to get the best move
         if (node == null) { //if no move found from initial AStar call
             node = astar.AStar(state, thing, 1, heuristic); // try to avoid others and break out of deadlock
@@ -1048,16 +1047,75 @@ function start_simulation(max_gen, callback) {
     initialize_simulation();
     interval_id = setInterval(simulate_and_visualize, data.ms_between_updates);
     interval_id2 = setInterval(graph.simulate, data.ms_between_updates); //think these two values should be the same
+    ///this is testing out voronoi stuff
+    console.log('before the call to voronoi')
+    function randgp(max) {return Math.floor(Math.random()*max)}
+// HF#2 Random hex color
+function randhclr() {
+  return "#"+
+  ("00"+randgp(256).toString(16)).slice(-2)+
+  ("00"+randgp(256).toString(16)).slice(-2)+
+  ("00"+randgp(256).toString(16)).slice(-2)
 }
-// var _indexCounter = 0;
-// var encoder = function() {return;};
-// var context_list = [];
-// var encoder = function() {return;}
-// function GIF() {
-//     console.log('i called gif()')
-//     const encoder = GIFEncoder();
-// }
-// var encoder = GIFEncoder();
+// HF#3 Metrics: Euclidean, Manhattan and Minkovski 3/20/17
+var D = 1;
+var D2 = Math.sqrt(2);
+console.log('inside the call to voronoi')
+var heuristic = manhattand;  // default to manhattan
+	if (euclidean) {
+	    heuristic = euclideand;
+	} else if (diagonal) {
+	    heuristic = diagonald;
+	}
+console.log(heuristic)
+function Metric(x,y,heuristic) {
+var dx = Math.abs(x);
+var dy = Math.abs(y);
+console.log('inside metric function')
+  if(heuristic=='euclideand') {
+  	console.log('the heuristic is euclidean')
+  	return Math.sqrt(x*x + y*y)}
+  if(heuristic=='manhattand') {
+  	console.log('the heuristic is manhattan')
+  	return Math.abs(x) + Math.abs(y)}
+  // if(mt==3) {return(Math.pow(Math.pow(Math.abs(x),3) + Math.pow(Math.abs(y),3),0.33333))}
+  if(heuristic=diagonald) {
+  	console.log('the heuristic is diagonal')
+  	return (D * (dx + dy) + (D2 - 2 * D) * Math.min(dx, dy))}
+}
+// Plotting Voronoi diagram. aev 3/10/17
+function pVoronoiD() {
+  var cvs=document.getElementById("cvsId");
+  var ctx=cvs.getContext("2d");
+  var w=cvs.width, h=cvs.height;
+  var x=y=d=dm=j=0, w1=w-2, h1=h-2;
+  var n=data.max['Exit'];
+  // document.getElementById("sites").value; //would be num exits for us
+  var heuristic=heuristic
+  // document.getElementById("mt").value; //would be heuristic checkbox for us
+  var X=new Array(n), Y=new Array(n), C=new Array(n);
+  ctx.fillStyle="white"; ctx.fillRect(0,0,w,h);
+  for(var i=0; i<n; i++) {
+    X[i]=randgp(w1); Y[i]=randgp(h1); C[i]=randhclr();
+  }
+  for(y=0; y<h1; y++) {
+    for(x=0; x<w1; x++) {
+      dm=Metric(h1,w1,heuristic); j=-1;
+      for(var i=0; i<n; i++) {
+        d=Metric(X[i]-x,Y[i]-y,heuristic)
+        if(d<dm) {dm=d; j=i;}
+      }//fend i
+      ctx.fillStyle=C[j]; ctx.fillRect(x,y,1,1);
+    }//fend x
+  }//fend y
+  ctx.fillStyle="black";
+  for(var i=0; i<n; i++) {
+    ctx.fillRect(X[i],Y[i],3,3);
+  }
+}
+/////
+}
+
 take_snapshot_calls = 0;
     function simulate_and_visualize() {
       number_generations += 1;
@@ -1086,29 +1144,6 @@ take_snapshot_calls = 0;
         encoder.addFrame(context)
         encoder.finish();
         encoder.download("download.gif","image/gif");
-
-        // if(take_snapshot_calls = 0) {
-        //     encoder.start();
-
-        //     // encoder.addFrame(context)
-        //     take_snapshot_calls = take_snapshot_calls + 1;
-        // }
-        // else {
-        //     encoder.setProperties(true,true)
-        // }
-        // encoder.addFrame(context)
-        // if(current_population == 0) {
-        //     encoder.finish();
-        //     encoder.download("download.gif","image/gif");
-        // }
-        // encoder.finish();
-        // encoder.download("download.gif","image/gif");
-
-        // var canvas = document.getElementById('grid');
-        // for (var i=0; i<____; i++) {
-            // var canvas = document.getElementById('grid');
-            // var context = canvas.getContext('2d');
-            // context_list.push(context)
         }
 
     }
